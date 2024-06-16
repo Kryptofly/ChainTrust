7f62fdb32b54e2514b2345c15acea2162f1d996d
Cryptofly <156387500+Kryptofly@users.noreply.github.com>
2024-06-16 13:05:00 +0300

    Create Dashboard.js

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>ChainTrust Dashboard</h1>
+      <p>Overview of trust scores and reviews</p>
+    </div>
+  );
+};
+
+export default Dashboard;
